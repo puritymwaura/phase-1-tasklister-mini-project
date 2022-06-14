abc48f5bdfa6347db0e26e170cfddb113c9a32cb
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-06-14 01:49:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let text = document.querySelector('input')
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    addTodo(e.target['new-task-description'].value)
+    form.reset()
+  })
 });
+
+function addTodo(input){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  btn.addEventListener('click', deleteList)
+  li.textContent = `${input}  `
+  document.querySelector('#list').appendChild(li)
+  li.appendChild(btn)
+  
+}
+
+function deleteList(e){
+  e.target.parentNode.remove()
+}
